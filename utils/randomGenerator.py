7e9166f6e71a5f386d5132781cb679c3370760f1
sebifp <sebiflorinp@gmail.com>
2023-12-01 16:05:12 +0200

    ADD date and problem id generator

--- utils/randomGenerator.py
+++ utils/randomGenerator.py
@@ -7,11 +7,17 @@ class RandomGenerator:
 		"""
 		self.__ids = []
 		self.__problemIds = []
+		self.__days = []
+		self.__months = []
 		for id in range(1, 10000):
 			self.__ids.append(id)
 		for laboratoryNumber in range(1, 101):
 			for problemNumber in range(1, 1001):
 				self.__problemIds.append(str(laboratoryNumber) + "_" + str(problemNumber))
+		for day in range(1, 32):
+			self.__days.append(day)
+		for month in range(1, 13):
+			self.__months.append(month)
 		random.seed(2)
 
 	def generateId(self):
@@ -19,6 +25,20 @@ class RandomGenerator:
 		A function that generates a random id.
 		Preconditions: -
 		Post-conditions: a random integer between 0 and 9999.
+		Raises: Exception: There are no ids left.
+		"""
+		try:
+			randomId = random.choice(self.__ids)
+			self.__ids.remove(randomId)
+		except:
+			raise Exception("There are no ids left.")
+		return randomId
+	
+	def generateProbleId(self):
+		"""
+		A function that generates a random id for lab problems.
+		Preconditions: -
+		Post-conditions: a string with the following format: laboratoryNumber_problemNumber
 		Raises: Exception: There are no ids left.
 		"""
 		try:
@@ -48,6 +68,14 @@ class RandomGenerator:
 		for index in range(0, length):
 			word += random.choice(characters)
 		return word
+	
+	def generateRandomDate(self):
+		"""
+		A function that generates a random date with the following format: mm/dd/yyyy.
+		Preconditions: -
+		Post-conditions: a string with the following format: mm/dd/yyyy
+		"""
+		return str(random.choice(self.__months)) + "/" + str(random.choice(self.__days)) + "/" + str(random.random() * 10000)
 	
 	def generateNumber(self):
 		"""
